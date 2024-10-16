a59a7867ff16f60bd73527ae0a8fb03cc25400d7
Firstways <106992641+Firstways@users.noreply.github.com>
2024-10-16 11:25:25 +0200

    image en couleur et creation de la classe de teste et compilation des fichiers

--- ProjetASD3/src/App.java
+++ ProjetASD3/src/App.java
@@ -4,7 +4,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         Image img = new Image(1000, 1000);
-        img.setRectangle(200, 200, 1000, 1000, Color.getColor("RED"));
+        Color col = Color.BLUE;
+        img.setRectangle(0, 1000, 0, 1000,col );
         img.save("square");
     }
 }
---
+++ ProjetASD3/src/Test/TestQuadTree.java
@@ -0,0 +1,13 @@
+package Test;
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+public class TestQuadTree {
+    @Test
+    public void test_null_case(){
+
+
+        "a".equals("a");
+    }
+}
