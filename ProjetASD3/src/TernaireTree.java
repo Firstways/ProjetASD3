62c97f748632de4649c4bae5494ccc5d6c5e289e
deatheed <95500649+deatheed@users.noreply.github.com>
2024-11-19 12:16:29 +0100

    ternaire tree qui compile et qui s'execute

--- ProjetASD3/src/TernaireTree.java
+++ ProjetASD3/src/TernaireTree.java
@@ -1,149 +1,235 @@
+import java.awt.*;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.List;
 
 @SuppressWarnings("unused")
 
 public final class TernaireTree {
 
-    private TernaireTree Ouest;
-    private TernaireTree NordEst;
-    private TernaireTree SudEst;
+    private TernaireTree ouest;
+    private TernaireTree nordEst;
+    private TernaireTree sudEst;
 
-    private Point pt;
+    private Point point;
     private boolean isEmpty;
     private String region;
 
-    public TernaireTree(TernaireTree ouest, TernaireTree nordEst, TernaireTree sudEst, Point pt) {
-        this.Ouest = ouest;
-        this.NordEst = nordEst;
-        this.SudEst = sudEst;
-        this.pt = pt;
-        this.isEmpty = this.isEmpty();
+    public TernaireTree(TernaireTree ouest, TernaireTree nordEst, TernaireTree sudEst, Point point) {
+        this.ouest = ouest;
+        this.nordEst = nordEst;
+        this.sudEst = sudEst;
+        this.point = point;
+        this.isEmpty = this.is_empty();
         this.region = null;
     }
 
-    public TernaireTree(Point pt) {
-        this.Ouest = null;
-        this.NordEst = null;
-        this.SudEst = null;
-        this.pt = pt;
+    public TernaireTree(Point point) {
+        this.ouest = null;
+        this.nordEst = null;
+        this.sudEst = null;
+        this.point = point;
         this.isEmpty = true;
     }
 
     public TernaireTree getOuest() {
-        return this.Ouest;
+        return this.ouest;
     }
-
     public TernaireTree getNordEst() {
-        return this.NordEst;
+        return this.nordEst;
     }
-
     public TernaireTree getSudEst() {
-        return this.SudEst;
+        return this.sudEst;
+    }
+    public Point getPoint() {
+        return this.point;
     }
 
-    public Point getpt() {
-        return this.pt;
+    public String colorIdentifier(Color color ){
+        if (color.equals(Color.RED)) {
+            return "ROUGE";
+        } else if (color.equals(Color.BLUE)) {
+            return "BLEU";
+        } else if (color.equals(Color.GREEN)) {
+            return "VERT";
+        } else if (color.equals(Color.YELLOW)) {
+            return "JAUNE";
+        } else if (color.equals(Color.CYAN)) {
+            return "CYAN";
+        } else if (color.equals(Color.MAGENTA)) {
+            return "MAGENTA";
+        } else if (color.equals(Color.GRAY)) {
+            return "GRIS";
+        } else if (color.equals(Color.PINK)) {
+            return "ROSE";
+        } else if (color.equals(Color.ORANGE)) {
+            return "ORANGE";
+        } else {
+            return "INCONNU";
+        }
     }
 
-    public boolean isEmpty() {
-        return this.Ouest == null && this.NordEst == null && this.SudEst == null;
+    public Color getColorO(){
+        return this.getPoint().getColors()[0];
+    }
+    public Color getColorNe(){
+        return this.getPoint().getColors()[1];
+    }
+    public Color getColorSe(){
+        return this.getPoint().getColors()[2];
+    }
+
+
+    public String getColorOString() {
+        Color c = this.getPoint().getColors()[0];
+        return colorIdentifier(c);
+    }
+    public String getColorNeString() {
+        Color c = this.getPoint().getColors()[1];
+        return colorIdentifier(c);
+    }
+    public String getColorSeString() {
+        Color c = this.getPoint().getColors()[2];
+        return colorIdentifier(c);
+    }
+
+    public boolean is_empty(){
+        if ((this.ouest == null )&&(this.nordEst == null)&&(this.sudEst == null)){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     /*
-     * Recherche le noeud auquel appartient un pt donné.
+     * Recherche le noeud auquel appartient un point donné.
      */
-    public Dual searchTTree(Point ptEnfant) {
-        Point ptParent = this.getpt();
+    public Dual searchTTree(Point pointEnfant) {
+        Point pointParent = this.getPoint();
 
-        int X_parent = ptParent.getX();
-        int Y_parent = ptParent.getY();
+        int X_parent = pointParent.getX();
+        int Y_parent = pointParent.getY();
 
-        int X_enfant = ptEnfant.getX();
-        int Y_enfant = ptEnfant.getY();
+        int X_enfant = pointEnfant.getX();
+        int Y_enfant = pointEnfant.getY();
 
-        System.out.println("Coordonnées du pt parent : (" + X_parent + ", " + Y_parent + ")");
-        System.out.println("Coordonnées du pt enfant : (" + X_enfant + ", " + Y_enfant + ")");
+        System.out.println("Coordonnées du point parent : (" + X_parent + ", " + Y_parent + ")");
+        System.out.println("Coordonnées du point enfant : (" + X_enfant + ", " + Y_enfant + ")");
 
         if (X_enfant <= X_parent) {
-            if (this.Ouest != null) {
-                if (this.Ouest.getpt().equals(ptEnfant)) {
-                    return new Dual(this, "Ouest");
+            if (this.ouest != null) {
+                if (this.ouest.getPoint().equals(pointEnfant)) {
+                    return new Dual(this, "ouest");
                 } else {
-                    return this.getOuest().searchTTree(ptEnfant);
+                    return this.getOuest().searchTTree(pointEnfant);
                 }
             } else {
-                System.out.println("Ouest");
-                return new Dual(this, "Ouest");
+                System.out.println("ouest");
+                return new Dual(this, "ouest");
             }
         } else if (X_enfant > X_parent && Y_enfant <= Y_parent) {
-            if (this.NordEst != null) {
-                if (this.NordEst.getpt().equals(ptEnfant)) {
-                    return new Dual(this, "NordEst");
+            if (this.nordEst != null) {
+                if (this.nordEst.getPoint().equals(pointEnfant)) {
+                    return new Dual(this, "nordEst");
                 } else {
-                    return this.getNordEst().searchTTree(ptEnfant);
+                    return this.getNordEst().searchTTree(pointEnfant);
                 }
             } else {
-                System.out.println("NordEst");
-                return new Dual(this, "NordEst");
+                System.out.println("nordEst");
+                return new Dual(this, "nordEst");
             }
         } else if (X_enfant > X_parent && Y_enfant > Y_parent) {
-            if (this.SudEst != null) {
-                if (this.SudEst.getpt().equals(ptEnfant)) {
-                    return new Dual(this, "SudEst");
+            if (this.sudEst != null) {
+                if (this.sudEst.getPoint().equals(pointEnfant)) {
+                    return new Dual(this, "sudEst");
                 } else {
-                    return this.getSudEst().searchTTree(ptEnfant);
+                    return this.getSudEst().searchTTree(pointEnfant);
                 }
             } else {
-                System.out.println("SudEst");
-                return new Dual(this, "SudEst");
+                System.out.println("sudEst");
+                return new Dual(this, "sudEst");
             }
         } else {
-            throw new IllegalArgumentException("Région non trouvée. Vérifiez les coordonnées du pt.");
+            throw new IllegalArgumentException("Région non trouvée. Vérifiez les coordonnées du point.");
         }
     }
 
     /*
-     * Ajoute un pt au ternaire tree.
+     * Ajoute un point au ternaire tree.
      */
     public void addTTree(Point p) {
-        Dual regionRecherche = searchTTree(p);
+        Dual region_recherche = searchTTree(p);
 
-        switch (regionRecherche.region) {
-            case "Ouest":
-                regionRecherche.ternaire.Ouest = new TernaireTree(p);
-                break;
-            case "NordEst":
-                regionRecherche.ternaire.NordEst = new TernaireTree(p);
-                break;
-            case "SudEst":
-                regionRecherche.ternaire.SudEst = new TernaireTree(p);
-                break;
-            default:
-                throw new IllegalArgumentException("Erreur inconnue, contactez les programmeurs.");
+        if (region_recherche.region.equals("ouest")){
+            region_recherche.ternaire.ouest = new TernaireTree(p);
+        }else if (region_recherche.region.equals("nordEst")){
+            region_recherche.ternaire.nordEst = new TernaireTree(p);
+        }else if (region_recherche.region.equals("sudEst")){
+            region_recherche.ternaire.sudEst = new TernaireTree(p);
+        }else {
+            throw new IllegalArgumentException(" Erreur inconnue merci de prendre contact avec les programmeurs");
         }
     }
 
     /*
-     * Construit le ternaire tree entier à partir d'un tableau de pts.
+     * Construit le ternaire tree entier à partir d'un tableau de points.
      */
-    public void buildTTree(Point[] pts) {
-        for (int k = 1; k < pts.length; k++) {
-            System.out.println("pt " + k);
-            this.addTTree(pts[k]);
+    public void buildTTree(Point[] points) {
+        for (int k = 1; k < points.length; k++) {
+            System.out.println("point " + k);
+            this.addTTree(points[k]);
         }
     }
+
+
+    public void toImageEncaps(String filename,Image img,int Xmin, int Ymin, int Xmax, int Ymax){
+        if (this == null){
+            return;
+        }else { 
+            img.setRectangle(Xmin, Ymin, this.getPoint().getX(), this.getPoint().getY(), getColorO());
+            img.setRectangle( this.getPoint().getX(),Ymin,Xmax, this.getPoint().getY(), getColorNe());
+            img.setRectangle(this.point.getX(),this.point.getY(), Xmax, Ymax, getColorSe());
+           
+            if (this.ouest != null){
+                System.out.println("O");
+                this.ouest.toImageEncaps(filename, img, Xmin, Ymin, this.getPoint().getX(), this.getPoint().getY());
+            }
+
+            if (this.nordEst != null){
+                System.out.println("Ne");
+                this.nordEst.toImageEncaps(filename, img, this.getPoint().getX(), Ymin, Xmax, this.getPoint().getY());
+            }
+
+            if (this.sudEst != null){
+                System.out.println("Se");
+                this.sudEst.toImageEncaps(filename, img, this.getPoint().getX(), this.getPoint().getY(), Xmax, Ymax);
+            }         
+        }
+ 
+    }
+    /*
+     * Génère une image a partir du quadtree
+     * Attention 0,0 = haut gauche
+     */
+    public void toImage(String filename,Image img){
+      toImageEncaps(filename, img, 0, 0, img.width(), img.height());  
+    }
+
 
     /*
      * Représentation textuelle de l'arbre (parcours symétrique).
      */
     public void toText() {
         if (this.getOuest() == null) {
-            System.out.print("OUEST(" + this.getpt().getX() + "," + this.getpt().getY() + "), ");
+            System.out.print("OUEST(" + this.getPoint().getX() + "," + this.getPoint().getY() + "), ");
         }
         if (this.getNordEst() == null) {
-            System.out.print("NORDEST(" + this.getpt().getX() + "," + this.getpt().getY() + "), ");
+            System.out.print("NORDEST(" + this.getPoint().getX() + "," + this.getPoint().getY() + "), ");
         }
         if (this.getSudEst() == null) {
-            System.out.print("SUDEST(" + this.getpt().getX() + "," + this.getpt().getY() + "), ");
+            System.out.print("SUDEST(" + this.getPoint().getX() + "," + this.getPoint().getY() + "), ");
         }
 
         if (this.getOuest() != null) {
@@ -161,5 +247,54 @@ public final class TernaireTree {
             this.getSudEst().toText();
             System.out.print(")");
         }
+    }
+
+    public void reColor(Point p, Color color){
+        // appelle search colors
+        // modifie la colors
+        Dual dual = searchTTree(p);
+
+        if (dual.region=="ouest"){
+            this.getPoint().setColor(color,0);
+        }else if (dual.region=="nordEst"){
+            this.getPoint().setColor(color,1);
+
+        }
+        else if (dual.region=="sudEst"){
+            this.getPoint().setColor(color,2);
+
+        }else {
+            throw new IllegalArgumentException(" Erreur inconnue merci de prendre contact avec les programmeurs");
+        }
+        compressTTree(dual);
+    }
+
+    public void compressTTree(Dual dual){
+        Point p2 = this.getPoint();
+        if (dual.region=="ouest"){
+            if (dual.ternaire.is_empty()){
+                if ((dual.ternaire.getColorO()==dual.ternaire.getColorNe())&&(dual.ternaire.getColorSe()==dual.ternaire.getColorO())){
+                    dual.ternaire.ouest= null;
+                }
+            }
+           
+        }
+        else if (dual.region=="nordEst"){
+            if (dual.ternaire.is_empty()){
+                if ((dual.ternaire.getColorO()==dual.ternaire.getColorNe())&&(dual.ternaire.getColorSe()==dual.ternaire.getColorO())){
+                    dual.ternaire.nordEst= null;
+                }
+            }
+           
+        }else if (dual.region=="sudEst"){
+            if (dual.ternaire.is_empty()){
+                if ((dual.ternaire.getColorO()==dual.ternaire.getColorNe())&&(dual.ternaire.getColorSe()==dual.ternaire.getColorO())){
+                    dual.ternaire.sudEst= null;
+                }
+            }
+        }else {
+            throw new IllegalArgumentException(" Erreur inconnue merci de prendre contact avec les programmeurs");
+        }
+
     }
 }
