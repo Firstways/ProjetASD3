df89c0bce63e57e89b9f9782cb5baed112136e50
deatheed <95500649+deatheed@users.noreply.github.com>
2024-11-29 14:56:46 +0100

    Update App.java

--- ProjetASD3/src/App.java
+++ ProjetASD3/src/App.java
@@ -1,6 +1,4 @@
-import java.awt.Color;
-import java.util.*;
-
+import java.io.FileWriter;
 public class App {
 
     /*
@@ -11,7 +9,7 @@ public class App {
 Les armuments sont :
      */
     public static void main(String[] args) throws Exception {
-        
+        // verification des arguments en entrée
         if (args.length != 3){
             System.err.println("Fin du programme: Nombre d'argument incorrect");
             System.exit(1);
@@ -23,80 +21,91 @@ Les armuments sont :
         // Récuperation des arguments
         int strategie =  Integer.parseInt(args[0]);
         String entree = args[1]; // nom du fichier
-        String sortie = args[2];
-       
-       //Recuperation du fichier d'entrée et découpe
+        String sortie = args[2]; // nom du dossier de sortie
 
+        //----------------------------------------------------------------------------------------------------------
+        //Recuperation du fichier d'entrée,  et découpe
         if(strategie == 1){
-            MyFile myFile = new MyFile(entree);
+            MyFile myFile = new MyFile(entree,strategie);
             myFile.readFile();
             int image_size = myFile.getImageSize();
             Point[] point_add_to_quad= myFile.getPoints();
             Point[] region_to_recolor = myFile.getRecolors();
             int border_size = myFile.getBorderSize();
 
-            System.out.println(image_size);
-            for (Point p : point_add_to_quad){
-                System.out.println("X : "+p.getX()+ " Y : "+p.getY());
-                
-            }
-
-            for (Point p : region_to_recolor){
-                System.out.println("X : "+p.getX()+ " Y : "+p.getY());
-                
-            }
-            System.out.print(border_size);
+            // construction du QuadTree
             Quadtree quad = new Quadtree (point_add_to_quad[0]);
             quad.buildQTree(point_add_to_quad);
 
+            Image img = new Image(image_size, image_size);
+            quad.toImage(sortie+"/square_B", img, border_size);
+            try {
+                img.save(sortie+"boyenvalbelouinsargs_B");
+                FileWriter writer = new FileWriter(sortie+"boyenvalbelouinargs_B.txt");
+                String text_quad ="("+quad.toText()+")";
+                writer.write(text_quad);
+                writer.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             for (Point p : region_to_recolor){
+                System.out.print("points to recolor"+ p.toString());
+                System.out.println();
                 quad.reColor(p,p.getColor());
             }
-
-            Image img = new Image(image_size, image_size);
-            quad.toImage("square", img, 19);
+            img = new Image(image_size, image_size);
+            quad.toImage(sortie+"/square_R", img, border_size);
             try {
-                img.save("square");
-    
+                img.save(sortie+"boyenvalbelouinsargs_R");
+                FileWriter writer = new FileWriter(sortie+"boyenvalbelouinsargs_R.txt");
+                String text_quad ="("+quad.toText()+")";
+                writer.write(text_quad);
+                writer.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        if(strategie == 2){
-            MyFile myFile = new MyFile(entree);
+        //----------------------------------------------------------------------------------------------------------
+        else if(strategie == 2){
+            // Récuperation du fichiers d'entrée + découpe
+            MyFile myFile = new MyFile(entree, strategie);
             myFile.readFile();
             int image_size = myFile.getImageSize();
             Point[] point_add_to_ternaire= myFile.getPoints();
             Point[] region_to_recolor = myFile.getRecolors();
             int border_size = myFile.getBorderSize();
 
-            System.out.println(image_size);
-            for (Point p : point_add_to_ternaire){
-                System.out.println("X : "+p.getX()+ " Y : "+p.getY());
-                
-            }
 
-            for (Point p : region_to_recolor){
-                System.out.println("X : "+p.getX()+ " Y : "+p.getY());
-                
-            }
-            System.out.print(border_size);
-
+            // Contruction de TernaireTree 
             TernaireTree ternaire = new TernaireTree(point_add_to_ternaire[0]);
             ternaire.buildTTree(point_add_to_ternaire);
 
+            Image img = new Image(image_size, image_size);
+            ternaire.toImage(sortie+"/ternaire_B", img, border_size);
+            try {
+                img.save(sortie+"boyenvalbelouinsargs_B");
+                FileWriter writer = new FileWriter(sortie+"boyenvalbelouinargs_B.txt");
+                String text_ternaire ="("+ternaire.toText()+")";
+                writer.write(text_ternaire);
+                writer.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             for (Point p : region_to_recolor){
                 ternaire.reColor(p,p.getColor());
             }
-
-            Image img = new Image(image_size, image_size);
-            ternaire.toImage("ternaire", img, 19);
+            img = new Image(image_size, image_size);
+            ternaire.toImage(sortie+"/ternaire_R", img, border_size);
             try {
-                img.save("ternaire");
-    
+                img.save(sortie+"boyenvalbelouinsargs_R");
+                FileWriter writer = new FileWriter(sortie+"boyenvalbelouinsargs_R.txt");
+                String text_ternaire ="("+ternaire.toText()+")";
+                writer.write(text_ternaire);
+                writer.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
+           
     }
 }
